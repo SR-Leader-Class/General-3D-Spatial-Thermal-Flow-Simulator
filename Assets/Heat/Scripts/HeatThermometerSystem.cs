d74217fdf2c9d71689a0eea84c1e5b6e94faaddd
SR-Leader-Class <151819374+SR-Leader-Class@users.noreply.github.com>
2026-06-01 19:36:16 +0800

    Update HeatThermometerSystem.cs

--- Assets/Heat/Scripts/HeatThermometerSystem.cs
+++ Assets/Heat/Scripts/HeatThermometerSystem.cs
@@ -1,3 +1,11 @@
+/*
+ * Created :    Summer 2026
+ * Author :     蘇家賢
+ * Project :    General-3D-Spatial-Thermal-Flow-Simulator
+ * Filename :   HeatThermometerSystem.cs
+ * 
+ */
+
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
